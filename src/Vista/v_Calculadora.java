0dc3be5f28e127fda38271c06af2f33f1a3833c2
MartiHui <martiyanghui@gmail.com>
2018-11-28 11:58:26 +0100

    Woops. Olvidado guardar antes de commit

--- src/Vista/v_Calculadora.java
+++ src/Vista/v_Calculadora.java
@@ -7,13 +7,18 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Rectangle;
+import javax.swing.JTextField;
+import java.awt.Color;
 
 public class v_Calculadora extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	public v_Calculadora() {
 		prepareFrame();
+		createPantalla();
 	}
 	
 	// Crea la ventana principal de la calculadora y ajusta su tamaño
@@ -28,5 +33,14 @@ public class v_Calculadora extends JFrame {
 		
 		pack(); // Para que el tamaño de la ventana se ajuste al panel que contiene la calculadora
 	}
-
+	
+	// Crea las pantallas donde se mostrarán los inputs del usuario y el restulado de las operaciones
+	private void createPantalla() {
+		textField = new JTextField();
+		textField.setBounds(0, 0, 300, 25);
+		textField.setAlignmentY(RIGHT_ALIGNMENT); 
+		textField.setBackground(new Color(0xF3F3F3));
+		textField.setEditable(false); // Solo se puede manejar las pantallas mediante los botones
+		contentPane.add(textField);
+	}
 }
